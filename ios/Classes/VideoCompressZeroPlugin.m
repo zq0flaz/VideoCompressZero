0ebf7c23d71494009b455b024ea227e3345fcbca
flazmint <zq0flaz777@gmail.com>
2025-08-04 15:38:35 +0700

    ios

--- ios/Classes/VideoCompressZeroPlugin.m
+++ ios/Classes/VideoCompressZeroPlugin.m
@@ -1,7 +1,7 @@
 #import "VideoCompressPlugin.h"
 #import <video_compress_zero/video_compress_zero-Swift.h>
 
-@implementation VideoCompressZero
+@implementation VideoCompressZeroPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftVideoCompressZeroPlugin registerWithRegistrar:registrar];
 }
